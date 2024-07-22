0dd5f9d869d296687ffe09e3444ac38988156975
rajsameer6534 <rajsameer123p@gmail.com>
2024-07-22 19:30:32 +0530

    first commit

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,76 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Learning React",
+    body: "Hi Friends, I am learning React +Redux from youtube. It is going to be my first Social Media app Created Using react",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["Study", "Work Hard", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "NITR",
+    body: "Life at Nitr is awesome,one can achieve what he/she want to be",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelievable"],
+  },
+];
+
+export default PostListProvider;
